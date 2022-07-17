e45ccaddc37261cef67a28cbe925600a4720de7d
malico <hi@malico.me>
2022-07-17 20:15:22 +0100

    hash

---
+++ hash.rb
@@ -0,0 +1,11 @@
+colors = {
+  'red' => '#FF0000', 
+  'green' => '#00FF00', 
+  'blue' => '#0000FF'
+}
+
+colors.each do |color, colorCode|
+  print "The color #{color} has code #{colorCode}\n"
+end
+
+
